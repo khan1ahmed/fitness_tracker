2dfb587728f638c4a80c37b7662743c6bb81aea4
Ahmed <akhan5544266@gmail.com>
2023-12-23 13:01:00 +0500

    sets

--- lib/Screens/Signinscreen.dart
+++ lib/Screens/Signinscreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Loginscreen.dart';
+
 class Signinscreen extends StatefulWidget {
   const Signinscreen({super.key});
 
@@ -10,39 +12,122 @@ class Signinscreen extends StatefulWidget {
 class _SigninscreenState extends State<Signinscreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(backgroundColor: Color.fromARGB(255, 240, 245, 245),
+    return  Scaffold(backgroundColor:Colors.black,
       body: SafeArea(
-        child: Center(
-          child: Column(children: [
-           const SizedBox(height:30),
-            Icon(Icons.lock,size: 100,),
-            SizedBox(height:20),
-            Text("welcome back Fitness Tracker",
-            style: TextStyle(fontSize: 16,fontWeight: FontWeight.w800,color: Colors.grey),),
-            SizedBox(height:20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
+         child:Padding(
+           padding:  EdgeInsets.all(20),
+           child: Column(mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+               Text('data',style: TextStyle(fontSize: 17,color: Color(0xffFC9E12),),),
+              SizedBox(height: 10,),
+               Text('data',style:TextStyle(color: Color(0xffFC9E12),fontSize: 17,) ,),
+              SizedBox(height: 40,),
+              Text('Username',style:TextStyle(color: Color(0xffFC9E12),) ,),
+              SizedBox(height: 10,),
+              TextField(
                 decoration: InputDecoration(
-                enabledBorder:OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300),),
-                fillColor:const Color.fromARGB(255, 219, 216, 216),
-                filled: true,
-              ),),
-            ),
-            SizedBox(height:20),
-             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
+                 hintText: 'softxion',
+                 hintStyle: TextStyle(color: Color(0xffFC9E12),),
+
+                 border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xffFC9E12),
+                  
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+           
+                 ),
+           
+                ),
+              ),
+               SizedBox(height: 20,),
+           
+                              Text('Email',style:TextStyle(color: Color(0xffFC9E12),) ,),
+
+              SizedBox(height: 10,),
+              TextField(
                 decoration: InputDecoration(
-                enabledBorder:OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300),),
-                fillColor:const Color.fromARGB(255, 219, 216, 216),
-                filled: true,
-              ),),
-            ),
-          ],),
-        ),
+                 hintText: 'softxion@gmail.com',
+                 hintStyle: TextStyle(color: Color(0xffFC9E12),),
+
+                 border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color:  Color(0xffFC9E12),
+                  
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+           
+                 ),
+           
+                ),
+              ),
+               SizedBox(height: 20,),
+               Text('Password',style:TextStyle(color: Color(0xffFC9E12),) ,),
+               SizedBox(height: 10,),
+              TextField(
+                decoration: InputDecoration(
+                 hintText: '********',
+                 hintStyle: TextStyle(color: Color(0xffFC9E12),),
+
+                 border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xffFC9E12),
+                  
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+           
+                 ),
+           
+                ),
+              ),
+              SizedBox(height: 50,),
+              Center(
+                child: ElevatedButton(onPressed: (){},style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xffFC9E12)
+                ), child: Text('sign in')),
+              ),
+            //extra for next page from mw .asfand
+                   ElevatedButton(onPressed: (){ Navigator.push(context,
+      MaterialPageRoute(builder: (context)=>Loginscreen()));}, child: Text('data')),
+           
+           
+            ],
+           ),
+         ),
+        // Center(
+        //   child: Column(children: [
+        //    const SizedBox(height:30),
+        //     Icon(Icons.lock,size: 100,),
+        //     SizedBox(height:20),
+        //     Text("welcome back Fitness Tracker",
+        //     style: TextStyle(fontSize: 16,fontWeight: FontWeight.w800,color: Colors.grey),),
+        //     SizedBox(height:20),
+        //     Padding(
+        //       padding: EdgeInsets.symmetric(horizontal: 20),
+        //       child: TextField(
+        //         decoration: InputDecoration(
+        //         enabledBorder:OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        //         focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300),),
+        //         fillColor:const Color.fromARGB(255, 219, 216, 216),
+        //         filled: true,
+        //       ),),
+        //     ),
+        //     SizedBox(height:20),
+        //      Padding(
+        //       padding: EdgeInsets.symmetric(horizontal: 20),
+        //       child: TextField(
+        //         decoration: InputDecoration(
+        //         enabledBorder:OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        //         focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300),),
+        //         fillColor:const Color.fromARGB(255, 219, 216, 216),
+        //         filled: true,
+        //       ),),
+        //     ),
+        //   ],),
+        // ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,145 +1,39 @@
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:fitness_tracker/Screens/Signinscreen.dart';
-// import 'package:fitness_tracker/firebase_options.dart';
-// import 'package:flutter/material.dart';
-
-// void main()async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-// );
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//     @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//     home: MyApp(),
-//     //  home:  HomePage(),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//     );
-//   }
-// }
-
-
-import 'package:fitness_tracker/Screens/Signinscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fitness_tracker/firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:sensors_plus/sensors_plus.dart';
 
-void main() {
-  runApp(MyApp());
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
+  const MyApp({super.key});
+    @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: StepCounter(),
+    home: MyApp(),
+    //  home:  HomePage(),
     );
   }
 }
 
-class StepCounter extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
   @override
-  _StepCounterState createState() => _StepCounterState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _StepCounterState extends State<StepCounter> {
-  int stepCount = 0;
-  bool isPeak = false;
-
-  @override
-  void initState() {
-    super.initState();
-    accelerometerEvents.listen((AccelerometerEvent event) {
-      detectStep(event);
-    });
-  }
-
-  void detectStep(AccelerometerEvent event) {
-    double magnitude = event.x * event.x + event.y * event.y + event.z * event.z;
-
-    if (magnitude > 15.0) {
-      isPeak = true;
-    }
-
-    if (isPeak && magnitude < 11.0) {
-      setState(() {
-        stepCount++;
-        isPeak = false;
-      });
-    }
-  }
+class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Pedometer Example'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Steps Taken',
-                style: TextStyle(fontSize: 30),
-              ),
-              Text(
-                _steps,
-                style: TextStyle(fontSize: 60),
-              ),
-              Divider(
-                height: 100,
-                thickness: 0,
-                color: Colors.white,
-              ),
-              Text(
-                'Pedestrian Status',
-                style: TextStyle(fontSize: 30),
-              ),
-              Icon(
-                _status == 'walking'
-                    ? Icons.directions_walk
-                    : _status == 'stopped'
-                        ? Icons.accessibility_new
-                        : Icons.error,
-                size: 100,
-              ),
-              Center(
-                child: Text(
-                  _status,
-                  style: _status == 'walking' || _status == 'stopped'
-                      ? TextStyle(fontSize: 30)
-                      : TextStyle(fontSize: 20, color: Colors.red),
-                ),
-              ),
-              //extra for next page from me.asfand
-              ElevatedButton(onPressed: (){ Navigator.push(context,
-      MaterialPageRoute(builder: (context)=>Signinscreen()));}, child: Text('data')),
-            ],
-          ),
-        ),
-      ),
+    return Scaffold(
     );
   }
 }
