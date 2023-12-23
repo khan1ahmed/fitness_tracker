94441173f4a15f7187d888b6f5f507c3adcd964c
Ahmed <akhan5544266@gmail.com>
2023-12-23 12:42:07 +0500

    qwr

--- lib/main.dart
+++ lib/main.dart
@@ -1,161 +1,70 @@
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:fitness_tracker/Screens/Signinscreen.dart';
-// import 'package:fitness_tracker/firebase_options.dart';
-// import 'package:flutter/material.dart';
-
-// void main()async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-// );
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//     @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//     home: MyApp(),
-//     //  home:  HomePage(),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:pedometer/pedometer.dart';
-
-String formatDate(DateTime d) {
-  return d.toString().substring(0, 19);
-}
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StepCounter(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  late Stream<StepCount> _stepCountStream;
-  late Stream<PedestrianStatus> _pedestrianStatusStream;
-  String _status = '?', _steps = '?';
+class StepCounter extends StatefulWidget {
+  @override
+  _StepCounterState createState() => _StepCounterState();
+}
+
+class _StepCounterState extends State<StepCounter> {
+  int stepCount = 0;
+  bool isPeak = false;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  void onStepCount(StepCount event) {
-    print(event);
-    setState(() {
-      _steps = event.steps.toString();
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      detectStep(event);
     });
   }
 
-  void onPedestrianStatusChanged(PedestrianStatus event) {
-    print(event);
-    setState(() {
-      _status = event.status;
-    });
-  }
+  void detectStep(AccelerometerEvent event) {
+    double magnitude = event.x * event.x + event.y * event.y + event.z * event.z;
 
-  void onPedestrianStatusError(error) {
-    print('onPedestrianStatusError: $error');
-    setState(() {
-      _status = 'Pedestrian Status not available';
-    });
-    print(_status);
-  }
+    if (magnitude > 15.0) {
+      isPeak = true;
+    }
 
-  void onStepCountError(error) {
-    print('onStepCountError: $error');
-    setState(() {
-      _steps = 'Step Count not available';
-    });
-  }
-
-  void initPlatformState() {
-    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
-    _pedestrianStatusStream
-        .listen(onPedestrianStatusChanged)
-        .onError(onPedestrianStatusError);
-
-    _stepCountStream = Pedometer.stepCountStream;
-    _stepCountStream.listen(onStepCount).onError(onStepCountError);
-
-    if (!mounted) return;
+    if (isPeak && magnitude < 11.0) {
+      setState(() {
+        stepCount++;
+        isPeak = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Pedometer Example'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Steps Taken',
-                style: TextStyle(fontSize: 30),
-              ),
-              Text(
-                _steps,
-                style: TextStyle(fontSize: 60),
-              ),
-              Divider(
-                height: 100,
-                thickness: 0,
-                color: Colors.white,
-              ),
-              Text(
-                'Pedestrian Status',
-                style: TextStyle(fontSize: 30),
-              ),
-              Icon(
-                _status == 'walking'
-                    ? Icons.directions_walk
-                    : _status == 'stopped'
-                        ? Icons.accessibility_new
-                        : Icons.error,
-                size: 100,
-              ),
-              Center(
-                child: Text(
-                  _status,
-                  style: _status == 'walking' || _status == 'stopped'
-                      ? TextStyle(fontSize: 30)
-                      : TextStyle(fontSize: 20, color: Colors.red),
-                ),
-              )
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Step Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Step Count:',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '$stepCount',
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
